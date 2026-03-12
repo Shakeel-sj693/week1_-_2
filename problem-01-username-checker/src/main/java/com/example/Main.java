355398dd2dfcb44de3a5f5f82078fd2de19f4b88
SHAKEEL SJ <ss1279@srmist.edu.in>
2026-03-12 08:53:12 +0530

    Add Main.java for demo

---
+++ problem-01-username-checker/src/main/java/com/example/Main.java
@@ -0,0 +1,8 @@
+// Java code for checking username availability and providing suggestions
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        // Demo code
+    }
+}
